f15084122a5d4868c297e9fdd894e2a9b0d0d2d5
Win Khant Paing <winkhantpaing32@gmail.com>
2024-04-17 17:08:51 +0700

    migration test completed

--- database/migrations/task9/2021_11_09_090018_rename_name_in_companies_table.php
+++ database/migrations/task9/2021_11_09_090018_rename_name_in_companies_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class RenameNameInCompaniesTable extends Migration
@@ -16,6 +17,7 @@ class RenameNameInCompaniesTable extends Migration
         // TASK: write the migration to rename the column "title" into "name"
         Schema::table('companies', function (Blueprint $table) {
             // Write code here
+            DB::statement("ALTER TABLE companies CHANGE COLUMN title name VARCHAR(100)");
         });
     }
 
